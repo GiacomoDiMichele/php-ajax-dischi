5098aa3ce3cc03ec7ae103a62efd6fef05deb2f7
Giacomo <giacomo.1936@gmail.com>
2020-12-17 17:26:28 +0100

    test album dischi-php

--- php-dischi/index.php
+++ php-dischi/index.php
@@ -3,11 +3,32 @@
 <html lang="en" dir="ltr">
     <head>
         <meta charset="utf-8">
+        <link rel="stylesheet" href="style.css">
         <title></title>
     </head>
     <body>
-        <?php foreach ($dischi as $singolo_album) {
-            echo $singolo_album['title'];
-        } ?>>
+        <main>
+            <div class="container">
+                <div class="lista_album">
+                    <?php foreach ($dischi as $singolo_album) { ?>
+                    <div class="singolo_album">
+                        <img src="<?php echo $singolo_album['poster'] ?>" alt="<?php $singolo_album['title'] ?>">
+                        <p>
+                            <?php echo $singolo_album['title'] ?>
+                        </p>
+                        <p class="author">
+                            <?php echo $singolo_album['author'] ?>
+                        </p>
+                        <p class="genre">
+                            <?php echo $singolo_album['genre'] ?>
+                        </p>
+                        <p class="year">
+                            <?php echo $singolo_album['year'] ?>
+                        </p>
+                        <?php } ?>
+                    </div>
+                </div>
+            </div>
+        </main>
     </body>
 </html>
