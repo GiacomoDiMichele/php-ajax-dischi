23b83e7745c8fd9b609d2536fc54576178be9951
Giacomo <giacomo.1936@gmail.com>
2020-12-18 16:53:06 +0100

    creazione laravel mix // handlebars jquery

---
+++ ajax-dischi/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    //leggo e prendo lo script per l'handlebars
+    const script_album = $('#album-template').html();
+    const template_script = Handlebars.compile(script_album);
+
+    //faccio la chiamata ajax per prendere i dati di ogni singolo album
+
+
+
+    
+}
